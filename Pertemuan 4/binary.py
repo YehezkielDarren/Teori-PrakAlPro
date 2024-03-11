1919c58a8787e96e5e177bb42df9b5a1c9b57460
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-03-11 19:47:50 +0700

    Update binary.py

--- Pertemuan 4/binary.py
+++ Pertemuan 4/binary.py
@@ -1,7 +1,17 @@
 def binary(number_list, target):
     awal=0
-    akhir=len(number_list)
-    tengah=(awal+akhir)//2
-    for i in range(awal,akhir):
+    akhir=len(number_list)-1
+    while awal<=akhir:
+        tengah=(awal+akhir)//2
+        tebak = number_list[tengah]
+        if target< tebak:
+            akhir= tengah-1
+        elif target> tebak:
+            awal=tengah +1
+        else:
+            return (f"indeks ke : {tengah}")
         
+        
+susun = sorted([3,7,6,4,2,5,1])
+print(binary(susun,4))
         
