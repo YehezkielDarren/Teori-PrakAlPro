642b48a4cb9f8855c617e9e31bf2b62ec0bf9a18
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-04-30 17:56:50 +0700

    Create setDanDict.py

---
+++ Pertemuan_11/setDanDict.py
@@ -0,0 +1,13 @@
+user={
+    "nama":"Yehezkiel Darren",
+    "umur":19,
+    "no_telp":"087782811055",
+    "tanggal_lahir":"28-Maret-2005"
+}
+print(user["no_telp"]) # pengaksesan value pada dictionary
+print(type(user)) #di indektifikasi sebagai dictionary
+a={18,90,3}
+print(type(a)) #di indektifikasi sebagai set
+# pengaksesan set dengan for loops
+for i in a:
+    print(i)
