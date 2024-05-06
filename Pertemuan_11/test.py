b531f5c4ba8d849e8acc01fe32783d5f491763af
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-05-06 18:53:45 +0700

    Update test.py

--- Pertemuan_11/test.py
+++ Pertemuan_11/test.py
@@ -1,24 +1,25 @@
-email_count = {}
+Mahasiswa_db=dict()
 
-def count_emails(file_name):
-    with open(file_name, 'r') as file:
-        for line in file:
-            if line.startswith('From: '):
-                email = line.split()[1]
-                email_count[email] = email_count.get(email, 0) + 1
+def isi_data(pilihan:int):
+    nim=input("masukkan nim anda : ")
+    if pilihan==1:
+        nama=input("masukkan nama anda : ").lower()
+        prodi=input("masukkan prodi anda : ").lower()
+        angkatan,kode_prodi=nim[2:4],nim[:2]
+        return nim,nama,prodi,angkatan,kode_prodi
+    return nim
 
-    print_histogram(email_count)
-    print_email_count(email_count)
+def tambahData(db,identitas):
+    db[identitas[0]]={
+        "Nama":identitas[1],
+        "Program Studi":identitas[2],
+        "Tahun Angkatan":identitas[3],
+        "Kode Prodi":identitas[4]
+    }
+    return db
 
-def print_histogram(email_count):
-    print('Histogram:')
-    for email, count in email_count.items():
-        print(f'{email:<30} {count}')
-
-def print_email_count(email_count):
-    print('\nEmail count:')
-    print(email_count)
-
-if __name__ == '__main__':
-    file_name = input('Enter the name of the file: ')
-    count_emails(file_name)
+pilihan=1
+for _ in range(2):
+    identitasMahasiswa=list(isi_data(pilihan))
+    Mahasiswa_db=tambahData(Mahasiswa_db,identitasMahasiswa)
+print(Mahasiswa_db)
