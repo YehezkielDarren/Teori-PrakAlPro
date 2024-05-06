87f69deb8f7dff267f2cc291a5978e62155d381e
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-05-06 21:22:05 +0700

    Update test.py

--- Pertemuan_11/test.py
+++ Pertemuan_11/test.py
@@ -1,25 +1,7 @@
-Mahasiswa_db=dict()
-
-def isi_data(pilihan:int):
-    nim=input("masukkan nim anda : ")
-    if pilihan==1:
-        nama=input("masukkan nama anda : ").lower()
-        prodi=input("masukkan prodi anda : ").lower()
-        angkatan,kode_prodi=nim[2:4],nim[:2]
-        return nim,nama,prodi,angkatan,kode_prodi
-    return nim
-
-def tambahData(db,identitas):
-    db[identitas[0]]={
-        "Nama":identitas[1],
-        "Program Studi":identitas[2],
-        "Tahun Angkatan":identitas[3],
-        "Kode Prodi":identitas[4]
-    }
-    return db
-
-pilihan=1
-for _ in range(2):
-    identitasMahasiswa=list(isi_data(pilihan))
-    Mahasiswa_db=tambahData(Mahasiswa_db,identitasMahasiswa)
-print(Mahasiswa_db)
+#data list
+nama=["darren","ndus","calvin"]
+umur=[19,20,20]
+user=dict()
+for i in range(min(len(nama),len(umur))):
+    user[nama[i]]=umur[i]
+print(user)
